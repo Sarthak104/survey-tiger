d00cbeb003a7fc494e8083546ddd2bb96238aa5e
Sarthak <sarthakbhaiji104@gmail.com>
2020-09-15 12:21:27 +0530

    Updates

---
+++ precache-manifest.ef75e9545b25dc0c973b9826e5026a5b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fbcdf3a1e224d47f1a2295894250a64",
+    "url": "/survey-tiger/index.html"
+  },
+  {
+    "revision": "9f0f0a2a7e3892be6076",
+    "url": "/survey-tiger/static/css/2.dc17f35a.chunk.css"
+  },
+  {
+    "revision": "dfc4c54cb23148202398",
+    "url": "/survey-tiger/static/css/main.b64ff135.chunk.css"
+  },
+  {
+    "revision": "9f0f0a2a7e3892be6076",
+    "url": "/survey-tiger/static/js/2.0de3ea26.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/survey-tiger/static/js/2.0de3ea26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfc4c54cb23148202398",
+    "url": "/survey-tiger/static/js/main.f47737a8.chunk.js"
+  },
+  {
+    "revision": "00d50d8d0149c6f30c2b",
+    "url": "/survey-tiger/static/js/runtime-main.01ec65ae.js"
+  },
+  {
+    "revision": "abd255ac557ff28bcb225a3f95196362",
+    "url": "/survey-tiger/static/media/logo.abd255ac.png"
+  }
+]);
